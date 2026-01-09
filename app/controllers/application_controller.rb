232470445520dec6990eafebdc37f49199992d9a
Eric <ericmansur13@gmail.com>
2026-01-09 00:53:52 -0300

    cadastro de usuario com foto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,3 +8,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio])
   end
 end
+
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio, :avatar])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 end
 
+class User < ApplicationRecord
+  has_one_attached :avatar
+
+  validates :username, presence: true, uniqueness: true
+end
+
 
