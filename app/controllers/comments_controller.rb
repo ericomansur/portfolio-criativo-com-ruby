daefcf1d3e6041e9c61e50364eff9696c13df3cd
Eric <ericmansur13@gmail.com>
2026-01-11 02:08:32 -0300

    ajax comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,24 @@
-# app/controllers/comments_controller.rb
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    project = Project.find(params[:project_id])
-    project.comments.create(
-      user: current_user,
-      body: params[:comment][:body]
-    )
-    redirect_back fallback_location: public_feed_path
+    @project = Project.find(params[:project_id])
+    @comment = @project.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to request.referer }
+      end
+    else
+      redirect_to request.referer, alert: "Erro ao comentar"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
