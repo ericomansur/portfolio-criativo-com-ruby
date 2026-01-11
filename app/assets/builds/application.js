64fdb5b9e15e1fda2982bacb1dd83686c7f5ffab
Eric <ericmansur13@gmail.com>
2026-01-11 03:53:06 -0300

    teste

---
+++ app/assets/builds/application.js
@@ -0,0 +1,12 @@
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+
+// app/javascript/application.js
+var require_application = __commonJS({
+  "app/javascript/application.js"() {
+  }
+});
+export default require_application();
+//# sourceMappingURL=/assets/application.js.map
